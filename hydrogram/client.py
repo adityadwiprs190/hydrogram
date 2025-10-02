9b0d78a6870283f303ccda46e449b364323c9de3
adityadwiprs190 <146068940+adityadwiprs190@users.noreply.github.com>
2025-10-02 21:54:09 +0700

    Update client.py

--- hydrogram/client.py
+++ hydrogram/client.py
@@ -208,9 +208,13 @@ class Client(Methods):
     #DEVICE_MODEL = f"{platform.python_implementation()} {platform.python_version()}"
     # SYSTEM_VERSION = "Snapdragon 865 5G"
     #SYSTEM_VERSION = f"{platform.system()} {platform.release()}"
-    APP_VERSION    = "Rext Pro Max 15.0"
-    DEVICE_MODEL   = "Rext Quantum Ultra X"
-    SYSTEM_VERSION = "HyperOS X 5G Snapdragon∞ 999"
+    # APP_VERSION    = "Rext Pro Max 15.0"
+    # DEVICE_MODEL   = "Rext Quantum Ultra X"
+    # SYSTEM_VERSION = "HyperOS X 5G Snapdragon∞ 999"
+    APP_VERSION    = "iOS 18.0"
+    DEVICE_MODEL   = "iPhone 17 Pro Max"
+    SYSTEM_VERSION = "iOS 18.0"
+
 
     LANG_CODE = "en"
 
